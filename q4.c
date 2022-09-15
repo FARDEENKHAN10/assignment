08ff3f6e055c4ad7f4552cb836299e5b1d468e9e
FARDEENKHAN10 <93277917+FARDEENKHAN10@users.noreply.github.com>
2022-09-15 13:13:56 +0530

    Add files via upload

---
+++ q4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    float a;
+    printf("Enter radius of circle");
+    scanf("%d",&r);
+    a=3.14*r;
+    printf("Area of circle is %f having the radius %d",a,r);
+    return 0;
+}
